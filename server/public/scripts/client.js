1df91bea3c20b9cef3472f816dc4947d023358c0
Eben Stine <ebenstine@gmail.com>
2021-07-31 23:09:08 -0500

    added functions to client

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -15,6 +15,75 @@ function onReady() {
     })
     
 }
+function addEquation() {
+    let newEquation = {
+        firstNumber: $('#numberOne').val(),
+        secondNumber: $('#numberTwo').val(),
+        operator: operator,
+    }
+    console.log('Adding equation', newEquation);
+    $.ajax({
+        method: 'POST',
+        url: '/calc',
+        data: newEquation
+    })
+        .then(function(response) {
+            getHistory();
+        })
+        .catch(function(error) {
+            console.log('Error from server', error);
+            alert('Equation NO GO');
+        })
+    clear();
+}
 
+function getHistory() {
+    $.ajax({
+        method: 'GET',
+        url: '/calc'
+    })
+        .then(function(response){
+            console.log('Response from server', response);
+            render(response);
+        })
+        .catch( function( error ){
+            console.log('Error from server', error);
+            alert('Sorry, could not get response. Try again later.');
+        })
+}
 
+function render(object) {
+    $('#output').empty();
+    $('#history').empty();
 
+    $('#output').append(`
+        <div>${object.result[object.result.length -1]}</div>
+        `)
+
+    for (let i = 0; i < object.history.length; i++) {
+        $('#history').append(`
+        <ul>${object.history[i]}</ul>
+        `) 
+    }
+}
+function add() {
+    operator = '+';
+    console.log('clicked', operator);
+}
+function subtract(){
+    operator = '-';
+    console.log('clicked', operator);
+}
+function multiply(){
+    operator = '*';
+    console.log('clicked', operator);
+}
+function divide(){
+    operator = '/';
+    console.log('clicked', operator);
+}
+function clear(){
+    $('#numberOne').val('');
+    $('#numberTwo').val('');
+    operator = 0;
+}
